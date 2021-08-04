ee701e3dc5b1d62c5abffb3afd0506d00e4bdeb9
pavstar619 <pavelx16@gmail.com>
2021-08-04 23:58:11 +0600

    did some js code

---
+++ game/js/game.js
@@ -0,0 +1,24 @@
+let score = 0;
+let level = 0;
+
+// Aeroplane, Avion, Avión
+// Bank - Banque - Banco
+// Good / Benefit - Bien - Bien
+// View - Vue - Vista
+// Other - Autre  - Otro
+// Dual - Deux - Dos
+// Circle - Cercle - Circulo
+// All / Total - Tout - Todo
+// Young / Juvenile - Jeune - Joven
+
+
+const word_list_answers = [
+    ['Aeroplane', 'Avion', 'Avión'],
+    ['Bank', 'Banque', 'Banco']
+]
+
+const word_list_buttons = [
+    ['Aeroplane', 'Car'],
+    ['Bank', 'Law Office']
+]
+
