17d06bd7f64962b24141abd9461e7b9131570454
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-08-04 19:37:01 -0400

    modular event listeners for 2 scenarios

--- game/js/game.js
+++ game/js/game.js
@@ -35,9 +35,9 @@ const word_list_images = [
 let image_iter = 0
 let word_list_answers_iter = 0
 
-document.querySelector('.game-level-result').textContent = 'Click Left Button to Test';
-
-document.querySelector('.left-btn').addEventListener('click', function () {
+// the event listener functions for different answers
+const correctButtonEvent = () => {
+    // the response for the correct answer
     document.querySelector('.score-output-p').textContent = String(score);
     document.querySelector('.level-output-p').textContent = String(level);
 
@@ -53,11 +53,26 @@ document.querySelector('.left-btn').addEventListener('click', function () {
     document.querySelector('#french').textContent = word_list_answers[word_list_answers_iter][1];
     document.querySelector('#spanish').textContent = word_list_answers[word_list_answers_iter][2];
     word_list_answers_iter += 1;
+};
+
+const wrongButtonEvent = () => {
+    // response for wrong answer
+    let level_image = document.querySelector('.main-image');
+    level_image.src = `images/words/${word_list_images[image_iter]}`;
+    image_iter += 1;
 
 
-    
-});
+    document.querySelector('#english').textContent = word_list_answers[word_list_answers_iter][0];
+    document.querySelector('#french').textContent = word_list_answers[word_list_answers_iter][1];
+    document.querySelector('#spanish').textContent = word_list_answers[word_list_answers_iter][2];
+    word_list_answers_iter += 1;
+}
 
+// wire up the events for each game
+document.querySelector('.game-level-result').textContent = 'Click Left Button to Test';
+
+document.querySelector('.left-btn').addEventListener('click', correctButtonEvent);
+document.querySelector('.right-btn').addEventListener('click', wrongButtonEvent);
 
 
 
