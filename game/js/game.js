a0a69885ae65884539c4b5d12bf481780a7aea3b
Violet Guo <13026263+violetguos@users.noreply.github.com>
2021-08-04 20:23:24 -0400

    Unify button event (#2)
    * general event listern for wrong or right working
    
    * the game now know which one's correct without us hardcoding
    
    Co-authored-by: Y. Violet Guo <violetguos@users.noreply.github.com>

--- game/js/game.js
+++ game/js/game.js
@@ -1,3 +1,4 @@
+
 let score = 0;
 let level = 1;
 
@@ -27,6 +28,14 @@ const word_list_images = [
     'bank.png'
 ]
 
+const word_img_answer_lut = {
+    "aeroplane": {
+        answer: ['Aeroplane', 'Avion', 'Avión'],
+        buttons: ['Aeroplane', 'Car'],
+        image: 'aeroplane.png'
+    }
+}
+
 // DOM Classes
 // score-output-p
 // level-output-p
@@ -35,44 +44,90 @@ const word_list_images = [
 let image_iter = 0
 let word_list_answers_iter = 0
 
-// the event listener functions for different answers
-const correctButtonEvent = () => {
-    // the response for the correct answer
+// go to the next button
+const goToNext =  () => {
     document.querySelector('.score-output-p').textContent = String(score);
     document.querySelector('.level-output-p').textContent = String(level);
+    image_iter += 1;
 
+    let level_image = document.querySelector('.main-image');
+    level_image.src = `images/words/${word_list_images[image_iter]}`;
+
+    word_list_answers_iter += 1;
+    document.querySelector('#english').textContent = word_list_answers[word_list_answers_iter][0];
+    document.querySelector('#french').textContent = word_list_answers[word_list_answers_iter][1];
+    document.querySelector('#spanish').textContent = word_list_answers[word_list_answers_iter][2];
+
+    // random sample which one's the correct one
+    if (Math.random() < 0.5){
+        document.querySelector('.left-btn').textContent = word_list_buttons[word_list_answers_iter][0];
+        document.querySelector('.right-btn').textContent = word_list_buttons[word_list_answers_iter][1];
+
+    }
+    else {
+        document.querySelector('.left-btn').textContent = word_list_buttons[word_list_answers_iter][1];
+        document.querySelector('.right-btn').textContent = word_list_buttons[word_list_answers_iter][0];
+
+    }
+}
+
+// the event listener functions for different answers
+const correctButtonEvent = (e) => {
+    // the event listener response for the correct answer
     level += 1;
     score += 2;
 
-    let level_image = document.querySelector('.main-image');
-    level_image.src = `images/words/${word_list_images[image_iter]}`;
-    image_iter += 1;
-
-
-    document.querySelector('#english').textContent = word_list_answers[word_list_answers_iter][0];
-    document.querySelector('#french').textContent = word_list_answers[word_list_answers_iter][1];
-    document.querySelector('#spanish').textContent = word_list_answers[word_list_answers_iter][2];
-    word_list_answers_iter += 1;
+    return goToNext(e);
 };
 
-const wrongButtonEvent = () => {
+const wrongButtonEvent = (e) => {
     // response for wrong answer
-    let level_image = document.querySelector('.main-image');
-    level_image.src = `images/words/${word_list_images[image_iter]}`;
     image_iter += 1;
 
+    let level_image = document.querySelector('.main-image');
+    level_image.src = `images/words/${word_list_images[image_iter]}`;
+
+    word_list_answers_iter += 1;
 
     document.querySelector('#english').textContent = word_list_answers[word_list_answers_iter][0];
     document.querySelector('#french').textContent = word_list_answers[word_list_answers_iter][1];
     document.querySelector('#spanish').textContent = word_list_answers[word_list_answers_iter][2];
-    word_list_answers_iter += 1;
+}
+
+const buttonAnswerCorrect = (btn) => {
+    const current_choice = document.querySelector(btn).innerText.toLowerCase();
+    const img_name = document.querySelector('.main-image').src;
+    if(img_name.includes(current_choice)){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const buttonClick = (event) => {
+    event.preventDefault();
+    const btnClass = event.target.classList[2];
+    const isCorrect = buttonAnswerCorrect('.'+btnClass);
+    if (isCorrect) {
+        console.log("answer is correct")
+        return correctButtonEvent(event);
+    }
+    else {
+        return wrongButtonEvent(event);
+    }
+ 
 }
 
 // wire up the events for each game
 document.querySelector('.game-level-result').textContent = 'Click Left Button to Test';
+document.querySelector('.left-btn').addEventListener('click', buttonClick);
+document.querySelector('.right-btn').addEventListener('click', buttonClick);
 
-document.querySelector('.left-btn').addEventListener('click', correctButtonEvent);
-document.querySelector('.right-btn').addEventListener('click', wrongButtonEvent);
+// beginning content
+document.querySelector('#english').textContent = word_list_answers[word_list_answers_iter][0];
+document.querySelector('#french').textContent = word_list_answers[word_list_answers_iter][1];
+document.querySelector('#spanish').textContent = word_list_answers[word_list_answers_iter][2];
 
 
 
