3c0f76328ef484b760acf321b644e948b539f4d6
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-08-04 20:07:48 -0400

    general event listern for wrong or right working

--- game/js/game.js
+++ game/js/game.js
@@ -1,3 +1,4 @@
+
 let score = 0;
 let level = 1;
 
@@ -27,6 +28,14 @@ const word_list_images = [
     'bank.png'
 ]
 
+const word_img_answer_lut = {
+    "aeroplane": {
+        answer: ['Aeroplane', 'Avion', 'Avión'],
+        buttons: ['Aeroplane', 'Car'],
+        image: 'aeroplane.png'
+    }
+}
+
 // DOM Classes
 // score-output-p
 // level-output-p
@@ -36,17 +45,19 @@ let image_iter = 0
 let word_list_answers_iter = 0
 
 // the event listener functions for different answers
-const correctButtonEvent = () => {
-    // the response for the correct answer
-    document.querySelector('.score-output-p').textContent = String(score);
-    document.querySelector('.level-output-p').textContent = String(level);
-
+const correctButtonEvent = (e) => {
+    // the event listener response for the correct answer
     level += 1;
     score += 2;
 
+    document.querySelector('.score-output-p').textContent = String(score);
+    document.querySelector('.level-output-p').textContent = String(level);
+
+
+    image_iter += 1;
+
     let level_image = document.querySelector('.main-image');
     level_image.src = `images/words/${word_list_images[image_iter]}`;
-    image_iter += 1;
 
 
     document.querySelector('#english').textContent = word_list_answers[word_list_answers_iter][0];
@@ -55,7 +66,7 @@ const correctButtonEvent = () => {
     word_list_answers_iter += 1;
 };
 
-const wrongButtonEvent = () => {
+const wrongButtonEvent = (e) => {
     // response for wrong answer
     let level_image = document.querySelector('.main-image');
     level_image.src = `images/words/${word_list_images[image_iter]}`;
@@ -68,11 +79,37 @@ const wrongButtonEvent = () => {
     word_list_answers_iter += 1;
 }
 
+const buttonAnswerCorrect = (btn) => {
+    const current_choice = document.querySelector(btn).innerText.toLowerCase();
+    const img_name = document.querySelector('.main-image').src;
+    if(img_name.includes(current_choice)){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const buttonClick = (event) => {
+    event.preventDefault();
+    const btnClass = event.target.classList[2];
+    const isCorrect = buttonAnswerCorrect('.'+btnClass);
+    if (isCorrect) {
+        console.log("answer is correct")
+        return correctButtonEvent(event);
+    }
+    else {
+        return wrongButtonEvent(event);
+    }
+ 
+}
+
 // wire up the events for each game
 document.querySelector('.game-level-result').textContent = 'Click Left Button to Test';
+document.querySelector('.left-btn').addEventListener('click', buttonClick);
+document.querySelector('.right-btn').addEventListener('click', buttonClick);
 
-document.querySelector('.left-btn').addEventListener('click', correctButtonEvent);
-document.querySelector('.right-btn').addEventListener('click', wrongButtonEvent);
+
 
 
 
