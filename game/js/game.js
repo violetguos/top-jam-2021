8ae2bc28c6c657d9d9f22a246f5ab1c1b6ccf98a
pavstar619 <pavelx16@gmail.com>
2021-08-05 12:58:40 +0600

    commented out some of violets function so that player wins every time, and can see solution at the bottom as well. Will comment them out afterwards

--- game/js/game.js
+++ game/js/game.js
@@ -36,16 +36,16 @@ const word_img_answer_lut = {
     }
 }
 
-// DOM Classes
-// score-output-p
-// level-output-p
+
 
 
 let image_iter = 0
 let word_list_answers_iter = 0
 
+
+
 // go to the next button
-const goToNext =  () => {
+const goToNext = () => {
     document.querySelector('.score-output-p').textContent = String(score);
     document.querySelector('.level-output-p').textContent = String(level);
     image_iter += 1;
@@ -59,7 +59,7 @@ const goToNext =  () => {
     document.querySelector('#spanish').textContent = word_list_answers[word_list_answers_iter][2];
 
     // random sample which one's the correct one
-    if (Math.random() < 0.5){
+    if (Math.random() < 0.5) {
         document.querySelector('.left-btn').textContent = word_list_buttons[word_list_answers_iter][0];
         document.querySelector('.right-btn').textContent = word_list_buttons[word_list_answers_iter][1];
 
@@ -76,8 +76,10 @@ const correctButtonEvent = (e) => {
     // the event listener response for the correct answer
     level += 1;
     score += 2;
+    document.querySelector('.languages-display').style.display = "block"; // initially hides 3 languages at bottom. Set to "block" to show
 
-    return goToNext(e);
+
+    // return goToNext(e); // commenting this out as the player should stay in the current level and check the 3 answers solutions at the bottom before going to next level
 };
 
 const wrongButtonEvent = (e) => {
@@ -97,7 +99,7 @@ const wrongButtonEvent = (e) => {
 const buttonAnswerCorrect = (btn) => {
     const current_choice = document.querySelector(btn).innerText.toLowerCase();
     const img_name = document.querySelector('.main-image').src;
-    if(img_name.includes(current_choice)){
+    if (img_name.includes(current_choice)) {
         return true;
     }
     else {
@@ -108,27 +110,37 @@ const buttonAnswerCorrect = (btn) => {
 const buttonClick = (event) => {
     event.preventDefault();
     const btnClass = event.target.classList[2];
-    const isCorrect = buttonAnswerCorrect('.'+btnClass);
+    const isCorrect = buttonAnswerCorrect('.' + btnClass);
     if (isCorrect) {
-        console.log("answer is correct")
+        console.log("answer is correct");
+        document.querySelector('.game-level-result').textContent = 'You Got This!'; // respond right answer
         return correctButtonEvent(event);
     }
     else {
-        return wrongButtonEvent(event);
+        document.querySelector('.game-level-result').textContent = 'Try Again'; // respond wrong answer
+        // return wrongButtonEvent(event); // commenting this out for now as we want player to win. So he can answer the level after seeing the solution. Can change this later.
     }
- 
+
+
+
+
 }
 
+
 // wire up the events for each game
-document.querySelector('.game-level-result').textContent = 'Click Left Button to Test';
+document.querySelector('.game-level-result').textContent = 'Let\'s Play';
 document.querySelector('.left-btn').addEventListener('click', buttonClick);
 document.querySelector('.right-btn').addEventListener('click', buttonClick);
+document.querySelector('.next-level-button').addEventListener('click', goToNext); // button to go to next level
+
 
 // beginning content
-document.querySelector('.languages-display').style.display = "none";
+document.querySelector('.languages-display').style.display = "none"; // initially hides 3 languages at bottom. Set to "block" to show
 document.querySelector('#english').textContent = word_list_answers[word_list_answers_iter][0];
 document.querySelector('#french').textContent = word_list_answers[word_list_answers_iter][1];
 document.querySelector('#spanish').textContent = word_list_answers[word_list_answers_iter][2];
+
+
 
 
 
